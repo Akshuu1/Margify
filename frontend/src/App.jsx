34bd5242b3ccea8fcada7f45b26f0c039fabc479
Akshuu1 <akshata.14feb@gmail.com>
2025-12-26 16:34:46 +0530

    made signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './index.css'
 import Home from './pages/Home'
 import { Login } from './pages/Login'
+import { Signup } from './pages/SignUp'
 
 function App() {
 
   return (
     <>
     {/* <Home /> */}
-    <Login />
+    {/* <Login /> */}
+    {/* <Signup /> */}
   </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
-            <h1 style = {{fontFamily: 'Kiona-Regular'}} className="text-[#E0E0E0] text-[15vw]" >Margify</h1>
+        <div className="flex justify-center items-center text-center h-screen w-screen">
+            <h1 style = {{fontFamily: 'Kiona-Regular'}} className="text-[#E0E0E0] text-[12rem]" >Margify</h1>
         </div>
     )
 }
