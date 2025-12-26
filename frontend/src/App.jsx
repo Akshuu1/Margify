db2b068d8170dcfc04127da71163f2c1fb8f221c
Akshuu1 <akshata.14feb@gmail.com>
2025-12-26 16:20:32 +0530

    created home page and login page and added font

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+import Home from './pages/Home'
+import { Login } from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+    {/* <Home /> */}
+    <Login />
+  </>
+  )
+}
+export default App
