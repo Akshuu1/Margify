d22b20d8c0ec6497b1c02f6174ad8b1a0d4d0d0d
Akshuu1 <akshata.14feb@gmail.com>
2025-12-30 14:52:34 +0530

    removed env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,7 @@ const moongose = require('mongoose')
 const connectDB = async () =>{
     try{
         await moongose.connect(process.env.MONGO_URL)
+        console.log('Database Conected')
     } catch(error){
         return error
     }
