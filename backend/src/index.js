cbf06c3e5451f1292e264204f7c9ca86e2a7ef91
Akshuu1 <akshata.14feb@gmail.com>
2026-01-02 02:03:27 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const { connectDB } = require("./config/db");
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const { connectDB } = require("./config/db")
 
-dotenv.config();
-connectDB();
+dotenv.config()
+connectDB()
 
-const app = express();
+const app = express()
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://margify.onrender.com", "https://margify-ten.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ["http://localhost:5173", "https://margify.onrender.com", "https://margify-ten.vercel.app"],
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// }
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/routes", require("./routes/routePlannerRoutes"));
+app.use(cors())
+app.use(express.json())
+app.use("/api/auth", require("./routes/authRoutes"))
+app.use("/api/routes", require("./routes/routePlannerRoutes"))
 
 app.get("/", (req, res) => {
-  res.send("Server Working");
-});
+  res.send("Server Working")
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log("Server Started on", PORT);
-});
+  console.log("Server Started on", PORT)
+})
